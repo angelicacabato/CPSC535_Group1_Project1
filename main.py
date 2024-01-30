237843cc162d56b3fe0eefb1b8c838052cfe1854
Angelica Cabato <58801573+angelicacabato@users.noreply.github.com>
2024-01-29 23:22:03 -0800

    first draft of counting_sort
    Does not work when 0 in in the list

--- main.py
+++ main.py
@@ -36,15 +36,62 @@ def quick_sort(arr, low, high):
         quick_sort(arr, pi + 1, high)  # sorting everything to the right of the
         # pivot
 
-# TODO: Linear Sorting
+# TODO: Counting Sort
 
+def counting_sort(arr):
+
+    # find max value
+    max_val = max(arr)
+    print(max_val)
+
+    temp_size = max_val + 1
+
+    #initalize a temp arr to the size of the input arr + 1
+    temp_arr = [0] * (temp_size)
+    sorted_arr = [0] * (temp_size)
+    print(temp_arr)
+    print(sorted_arr)
+
+    # Set temp[i] to equal the number of elements equal to i
+    for i in range(len(arr)):
+       temp_arr[arr[i]] += 1
+
+    print("break")
+
+    # Set temp[i] to equal the number of elements less than or equal to i
+    for i in range(1, temp_size):
+       temp_arr[i] = (temp_arr[i] + temp_arr[i-1])
+       print(temp_arr[i])
+
+    print("break")
+
+    # sort the list
+    for i in reversed(range(1, len(arr))):
+        print(temp_arr[arr[i]])
+        sorted_arr[temp_arr[arr[i]]] = arr[i]
+        temp_arr[arr[i]] -= 1
+
+
+    # Copy to input arr
+    for i in range(0, len(arr)):
+        arr[i] = sorted_arr[i] + 1
+        print(arr[i])
+
+    return arr
+
+
+# TODO: Radix Sort
+
+# TODO: Bucket Sort
 
 def main():
 
     try:
         arr = list(map(int, input("Enter numbers separated by spaces: ").split()))
-        quick_sort(arr, 0, len(arr) - 1)
-        print("Sorted Array: ", arr)
+
+        print("Sorted Array using Counting Sort: ", counting_sort(arr))
+        #quick_sort(arr, 0, len(arr) - 1)
+        #print("Sorted Array using Quick Sort: ", arr)
     except ValueError:
         print("Please enter only integers separated by spaces.")
 
